f22748a8c90286efb2501afa6733df02ea914d7e
kmaepa <kmaepa@2u.com>
2025-02-03 11:53:20 +0200

    chore: remove vault secrets

--- tests/test_common.py
+++ tests/test_common.py
@@ -8,6 +8,7 @@ from edx_argoutils import common
 from datetime import datetime, date
 from unittest.mock import patch
 
+
 def test_generate_dates():
     result = common.generate_dates(
         start_date='2020-01-01',
@@ -30,7 +31,7 @@ def test_get_unzipped_cartesian_product():
     )
     assert result == [
         (1, 1, 1, 2, 2, 2, 3, 3, 3),
-        ("a", "b", "c", "a", "b", "c", "a", "b", "c")
+        ("a", "b", "c", "a", "b", "c", "a", "b", "c") 
     ]
     
 
@@ -38,9 +39,11 @@ def test_valid_course_id():
     result = common.get_filename_safe_course_id("course-v1:BerkeleyX+CS198.SDC.1+1T2021")
     assert result == "BerkeleyX_CS198.SDC.1_1T2021"
 
+
 def test_invalid_course_id():
     result = common.get_filename_safe_course_id("BerkeleyX!CS198.SDC.1!1T2021")
     assert result == "BerkeleyX_CS198.SDC.1_1T2021"
+
 
 def test_generate_date_range():
     # Test Case 1: Custom date range (is_daily=False)
@@ -63,12 +66,12 @@ def test_generate_date_range():
         expected = [date(2025, 1, 26)]  # Two days before fixed_today
         assert result == expected, f"Expected {expected}, but got {result}"
 
-    #Test Case 3: True-up scenario (is_daily=False, no start_date and end_date)
+    # Test Case 3: True-up scenario (is_daily=False, no start_date and end_date)
     with patch('edx_argoutils.common.date') as mock_date:
         mock_date.today.return_value = fixed_today
 
         result = common.generate_date_range(is_daily=False)
-        expected = [date(2024, 12, d) for d in range(1, 32)]  #Last completed month
+        expected = [date(2024, 12, d) for d in range(1, 32)]  # Last completed month
         assert result == expected, f"Expected {expected}, but got {result}"
 
     # Test Case 4: Invalid parameters
