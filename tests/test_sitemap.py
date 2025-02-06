8209c30a330557adbe46bb5b821294c3f037aca4
Muhammad Usama <muhammad.usama@arbisoft.com>
2025-02-06 19:39:13 +0500

    chore: removed unused packages

--- tests/test_sitemap.py
+++ tests/test_sitemap.py
@@ -11,7 +11,7 @@ from edx_argoutils.sitemap import fetch_sitemap, fetch_sitemap_urls, write_sitem
 from datetime import datetime
 
 
-SCRAPED_AT = '2025-02-06'
+SCRAPED_AT = datetime.now().strftime('%Y-%m-%d')
 
 
 class TestSitemapTasks(unittest.TestCase):
@@ -35,7 +35,7 @@ class TestSitemapTasks(unittest.TestCase):
         # Expected output for the mock sitemap index response
         expected_output = ['https://www.foo.com/sitemap-0.xml', 'https://www.foo.com/sitemap-1.xml']
 
-        # Call the function (directly, without Prefect context)
+        # Call the function directly
         result = fetch_sitemap_urls(sitemap_index_url='dummy_url')
 
         # Check if the result matches the expected output
